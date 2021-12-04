4aefd36b4a0e53d8c5ca0838264d7ad362ca6721
akulova <akulovairen17@gmail.com>
2021-12-04 19:01:44 +0200

    я сделала вроде груши

---
+++ src/Pears.java
@@ -0,0 +1,10 @@
+public class Pears extends Trees {
+
+    public Pears(String name) {
+        super(name);
+    }
+
+    public void collect(){
+        System.out.println("собираю груши " + name);
+    }
+}
