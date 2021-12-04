5aba24f3ec512930fd92d7c30f0c3b405e1cf0a3
akulova <akulovairen17@gmail.com>
2021-12-04 19:01:21 +0200

    я сделала вроде

---
+++ src/Cherry.java
@@ -0,0 +1,11 @@
+public class Cherry extends Trees {
+
+    public Cherry(String name) {
+        super(name);
+    }
+
+    public void collect () {
+        System.out.println("я собираю вишни " + name);
+
+    }
+}
