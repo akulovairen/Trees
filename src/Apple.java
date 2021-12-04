9ab9e0b66f07c0ed1a116fbbcf94b2fe7990ece0
akulova <akulovairen17@gmail.com>
2021-12-04 19:01:09 +0200

    я сделала вроде

---
+++ src/Apple.java
@@ -0,0 +1,11 @@
+public class Apple extends Trees {
+
+    public Apple(String name) {
+        super(name);
+    }
+
+    public void collect () {
+        System.out.println("я собираю яблоки " + name);
+
+    }
+}
